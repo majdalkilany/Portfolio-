42e7ed49072a1446da31378a8593d4c1c026240a
hassanShakur <hass.shakurhassan@gmail.com>
2023-05-18 17:51:39 +0300

    Added javascript form validation

--- app.js
+++ app.js
@@ -14,3 +14,24 @@ document.querySelectorAll('.nav-links').forEach((i) => {
     menu.classList.remove('active');
   });
 });
+
+const form = document.getElementById('fs-frm');
+form.addEventListener('submit', validateForm);
+function validateForm(event) {
+  const emailInput = document.querySelector('input[name="email"]');
+  const email = emailInput.value.toLowerCase(); // Convert email to lower case
+  if (emailInput.value !== email) {
+    event.preventDefault();
+    const errorElement = document.createElement('p');
+    errorElement.textContent =
+      'Please enter the email in lower case.';
+    errorElement.style.color = 'red';
+    const existingError = document.getElementById('email-error');
+    if (existingError) {
+      existingError.remove();
+    }
+    const submitButton = document.getElementById('form-btn');
+    submitButton.insertAdjacentElement('beforebegin', errorElement);
+    errorElement.id = 'email-error';
+  }
+}
